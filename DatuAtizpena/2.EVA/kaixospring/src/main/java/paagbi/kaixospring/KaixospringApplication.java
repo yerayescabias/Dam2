5c4992b536647207b898c00fccadf0d0d21d281d
yerayescabias <escabias.yeray@uni.eus>
2024-12-02 12:57:26 +0100

    a

---
+++ DatuAtizpena/2.EVA/kaixospring/src/main/java/paagbi/kaixospring/KaixospringApplication.java
@@ -0,0 +1,14 @@
+package paagbi.kaixospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaixospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaixospringApplication.class, args);
+		System.out.println("kaixo spring");
+	}
+
+}
---
+++ DatuAtizpena/2.EVA/kaixospring/src/test/java/paagbi/kaixospring/KaixospringApplicationTests.java
@@ -0,0 +1,13 @@
+package paagbi.kaixospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaixospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
